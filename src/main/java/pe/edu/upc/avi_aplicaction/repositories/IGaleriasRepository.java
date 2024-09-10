e37e84902597886e36082ea3eb0aa63e359a6db6
LindaSaraiP <perezdelafuentesarai@gmail.com>
2024-09-10 00:31:21 -0500

    Arreglo de bug en Galerias

--- src/main/java/pe/edu/upc/avi_aplicaction/repositories/IGaleriasRepository.java
+++ src/main/java/pe/edu/upc/avi_aplicaction/repositories/IGaleriasRepository.java
@@ -4,10 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.avi_aplicaction.entities.Galerias;
 
-import java.util.List;
-
 @Repository
 public interface IGaleriasRepository extends JpaRepository<Galerias, Integer>{
 
-    List<Galerias> findByUsuarioId(int idUsuario);
 }
